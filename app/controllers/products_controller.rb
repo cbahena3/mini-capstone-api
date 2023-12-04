4755643fd95be04e26f396f59b8c99b531f30645
Cristian B <cris.bahena77@gmail.com>
2023-12-04 15:24:04 -0600

    updating things

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,12 @@
 class ProductsController < ApplicationController
+  def everything
+    all = Product.all
+    render json: {message: all}
+  end
+
+  #the first one
+  def individual
+    first = Product.first
+    render json: {message: first}
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+product = Product.new(
+  name: "Dumbbell",
+  price: 50,
+  image_url:"https://www.ritfitsports.com/cdn/shop/products/ritfit-rubber-hex-dumbbell-set-10-60-lbs-weight-ritfit-613242.jpg?v=1669948018",
+  description:"Rubber hex dumbbells feature an ergonomically designed chrome handle—tapering at two ends—to enhance a power grip. The knurled handle surface provides solid contact with your hands, and you shall find it easy to fasten onto the hex dumbbells at any weight and under sweat."
+)
+
+product.save
