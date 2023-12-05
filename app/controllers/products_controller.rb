fe1fce7b78bd3c61139dc3c4779daa06fd6b0bca
Cristian B <cris.bahena77@gmail.com>
2023-12-05 12:47:31 -0600

    used post

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,18 @@ class ProductsController < ApplicationController
   end
 
   def show #one at a time
-    @products = Product.find_by(id: params[:id])
+    @product = Product.find_by(id: params[:id])
+    render template: "products/show"
+  end
+
+  def create
+    @product = Product.new(
+      name: "Lat Pull-Down",
+      price: 499,
+      image_url: "https://www.bellsofsteel.us/wp-content/uploads/2022/10/lat-pulldown-9.webp",
+      description: "Perform lat pulldowns, low rows, curls & more (up to 310 pounds) with this 2-in-1 machine!"
+    )
+    @product.save
     render template: "products/show"
   end
 end
