9d4a2607b88ae6fadec36f194f014a5d81e4eb68
Cristian B <cris.bahena77@gmail.com>
2023-12-05 11:51:45 -0600

    tried to add new controller/routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,11 @@
 class ProductsController < ApplicationController
+  def index #shows all
+    @products = Product.all
+    render template: "products/index"
+  end
 
+  def show #one at a time
+    @products = Product.find_by(id: params[:id])
+    render template: "products/show"
+  end
 end
