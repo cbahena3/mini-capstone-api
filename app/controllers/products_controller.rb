f6bf161d05e27e6769842b245f2631863d413660
Cristian B <cris.bahena77@gmail.com>
2023-12-05 11:25:53 -0600

    deleted routes and controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,3 @@
 class ProductsController < ApplicationController
-  def everything
-    all = Product.all
-    render json: {message: all}
-  end
 
-  #the first one
-  def individual
-    first = Product.first
-    render json: {message: first}
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,2 @@
 Rails.application.routes.draw do
-  get "/all_products" => "products#everything"
-  get "/individual" => "products#individual"
 end
