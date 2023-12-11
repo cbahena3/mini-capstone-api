e11d43bac68647a7bbbf0f8d550f3fafd6e4235f
Cristian B <cris.bahena77@gmail.com>
2023-12-11 14:04:25 -0600

    added supplier, supplier ID to product and jbuilder

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,29 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
-product = Product.new(
-  name: "Dumbbell",
-  price: 50,
-  image_url:"https://www.ritfitsports.com/cdn/shop/products/ritfit-rubber-hex-dumbbell-set-10-60-lbs-weight-ritfit-613242.jpg?v=1669948018",
-  description:"Rubber hex dumbbells feature an ergonomically designed chrome handle—tapering at two ends—to enhance a power grip. The knurled handle surface provides solid contact with your hands, and you shall find it easy to fasten onto the hex dumbbells at any weight and under sweat."
-)
+# product = Product.new(
+#   name: "Dumbbell",
+#   price: 50,
+#   image_url:"https://www.ritfitsports.com/cdn/shop/products/ritfit-rubber-hex-dumbbell-set-10-60-lbs-weight-ritfit-613242.jpg?v=1669948018",
+#   description:"Rubber hex dumbbells feature an ergonomically designed chrome handle—tapering at two ends—to enhance a power grip. The knurled handle surface provides solid contact with your hands, and you shall find it easy to fasten onto the hex dumbbells at any weight and under sweat."
+# )
 
-product.save
+# product.save
+
+# supplier = Supplier.create(
+#   name: "Rogue Fitness",
+#   email: "rogue@fitness.com",
+#   phone_number: "800-888-7777"
+# )
+
+# supplier = Supplier.create(
+#   name: "Raw Gear",
+#   email: "raw@gear.com",
+#   phone_number: "800-111-2222"
+# )
+
+# supplier = Supplier.create(
+#   name: "Bum Fitness",
+#   email: "bum@fitness.com",
+#   phone_number: "800-333-4444"
+# )
