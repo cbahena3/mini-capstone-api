2b0fa9f66009213550132956afa293153ca94b59
Cristian B <cris.bahena77@gmail.com>
2023-12-11 12:39:36 -0600

    added create and update tests new

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ApplicationRecord
   validates :price, presence: true
   validates :price, numericality: { greater_than: 0 }
   validates :description, presence: true
+  validates :description, length: {in: 10..500}
 
   def is_discounted?
     if price <= 10
