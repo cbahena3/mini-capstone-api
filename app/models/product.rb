d2048d4369d052d89eaf2f1fab8dfd0bd27e934b
Cristian B <cris.bahena77@gmail.com>
2023-12-11 11:35:57 -0600

    added validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, presence: true
+
   def is_discounted?
     if price <= 10
       return true
